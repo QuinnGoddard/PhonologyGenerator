33a51714cf56ca23a7e0bc7682c1a72d881c7900
QuinnGoddard <qnn.gddrd@gmail.com>
2020-05-17 17:25:41 -0600

    Update phonGenerator.js

--- phonGenerator.js
+++ phonGenerator.js
@@ -1,11 +1,13 @@
 let selectedV = new Set()
 let selectedC = new Set()
 
+// Generate list of selected vowels
 function handleVowelClick(event) {
   selectedV.add(event.target.value)
   document.getElementById("vSelection").innerHTML = Array.from(selectedV)
 }
 
+// Generate list of selected consonants
 function handleConsClick(event) {
   selectedC.add(event.target.value)
   document.getElementById("cSelection").innerHTML = Array.from(selectedC)
@@ -14,7 +16,8 @@ function handleConsClick(event) {
 function run(event) {
   var listLength = document.getElementById("numWords").value
   var length = document.getElementById("wordlength").value
-  wordList(listLength, Array.from(selectedV), Array.from(selectedC), length)
+  var syll = document.getElementbyName("syllable").value
+  wordList(listLength, Array.from(selectedV), Array.from(selectedC), length, syll)
 }
 
 /* var vowels = {
@@ -38,26 +41,97 @@ var consonants = {
 };
 */
 
-function makeWord(selectV, selectC, length) {
-    var phonemes = selectV.concat(selectC);
-    console.log("makeWord:", selectV, selectC, length)
-    let word = "";
-    let phone = "";
-    for (let i = 0; i < length; i++) {
-      let num = Math.floor((Math.random() * phonemes.length))
-      console.log("num", num)
-      phone = phonemes[num];
-      word += phone;     
-    };
+/* Select a random phoneme from a set of phonemes */
+function randomPhone(phones) {
+    let phone = ""
+    let num = Math.floor((Math.random() * phonemes.length))
+    phone = phones[num]
+    return phone
+}
+
+/* Make a syllable according to desired structure 
+    syll: desired syllable structure (V, CV, CVC, CCVCC)
+    c: set of consonants
+    v: set of vowels
+*/
+function makeSyll(syll, c, v) {
+    let syllable = ""
+    if syll == "CV" {
+       syllable += randomPhone(c)
+       syllable += randomPhone(v)
+    } else if syll == "CVC" {
+       syllable += randomPhone(c)
+       syllable += randomPhone(v)
+       syllable += randomPhone(c)
+    } else {
+       syllable += randomPhone(c)
+       syllable += randomPhone(c)
+       syllable += randomPhone(v)
+       syllable += randomPhone(c)
+       syllable += randomPhone(c)
+    }
+
+    return syllable
+}
+
+/* Compose a word according to desired parameters:
+    selectV: array of selected vowels
+    selectC: array of selected consonants
+    length: length of word (number of phonemes in word)
+    syll: syllable structure (either CV, CVC, or CCVCC)
+*/
+function makeWord(selectV, selectC, length, syll) {
+    //var phonemes = selectV.concat(selectC);
+    let word = ""
+    let phone = ""
+    
+    // make a word with (C)V syllables
+    if syll == "CV" {
+      if length%2 == 0 {
+        for (let i = 0; i < length/2; i++) {
+          word+=makeSyll("CV")
+        }
+      } else {
+        word += randomPhone(selectV)
+        for (let i = 0; i < (length-1)/2; i++) {
+          word+=makeSyll("CV")
+        }
+      }
+    // make a word with CVC syllables
+    } else if syll == "CVC" {
+      if length%3 == 0 {
+        for (let i = 0; i < length/3; i++) {
+          word+=makeSyll("CVC")
+        }
+      // if not divisible by 3, initial syllable is V
+      } else if length%3 == 1 {
+        word += randomPhone(selectV)
+        for (let i = 0; i < length/3; i++) {
+          word+=makeSyll("CVC")
+        }
+      // if not divisible by 3, initial syllable is CV
+      } else {
+        word+=makeSyll("CV")
+        for (let i = 0; i < length/3; i++) {
+          word+=makeSyll("CVC")
+        }
+      }
+    }
     return word;      
 }
 
-function wordList(n, selectV, selectC, length) {
+/* Generate a list of words according to desired properties:
+    n: number of words in word list
+    selectV: array of selected vowels
+    selectC: array of selected consonants
+    length: length of word (number of phonemes in word)
+    syll: syllable structure (either CV, CVC, or CCVCC)
+*/
+function wordList(n, selectV, selectC, length, syll) {
   var wordList = [];
   let word = "";
   for (let i = 0; i < n; i++) {
     word = makeWord(selectV, selectC, length);
-    console.log("word" + word)
     wordList.push(word);
   }
   let listHTML = ""
