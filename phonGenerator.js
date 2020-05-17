c9ab105a832e5a74cbd5b8c2d525d00318928619
QuinnGoddard <qnn.gddrd@gmail.com>
2020-05-17 10:27:25 -0600

    Update phonGenerator.js

--- phonGenerator.js
+++ phonGenerator.js
@@ -1,3 +1,36 @@
 function handleClick(event) {
   console.log(event)
 }
+
+var vowels = {
+    "a": ["low"],
+    "i": ["high"],
+    "o": ["mid", "round"],
+    "u": ["high", "round"],
+    "e": ["mid"],
+    "y": ["high", "round"]
+};
+
+var consonants = {
+    "k": ["velar", "stop"],
+    "g": ["velar", "voiced", "stop"],
+    "p": ["bilabial","stop"],
+    "b": ["bilabial", "voiced","stop"],
+    "t": ["alveolar", "stop"],
+    "d": ["alveolar", "voiced","stop"]
+    "s": ["alveolar", "fricative"],
+    "z": ["alveolar", "voiced","fricative"]
+};
+
+
+function makeWord(selectV, selectC, length) {
+    var phonemes = selectV.concat(selectC);
+    let word = "";
+    let phone = "";
+    for (let i = 0; i < length; i++) {
+      let num = Math.floor((Math.random() * phonemes.length-1) +0);
+      phone = phonemes.indexOf(num);
+      word.concat(phone);     
+      };
+    return word;      
+}
